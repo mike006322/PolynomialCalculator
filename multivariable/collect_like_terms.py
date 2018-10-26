7915003ac47d0c1f99cb6a08ba0e0783be516a91
Michael Angel <mike006322@gmail.com>
2018-10-26 15:17:45 -0700

    corrected function to stop altering the input

--- multivariable/collect_like_terms.py
+++ multivariable/collect_like_terms.py
@@ -4,18 +4,18 @@ def collect_like_terms(termMatrix):
     output is another termMatrix with terms collected
     termMatrix = [[" ", variable1, variable2...], [coefficient, exponent1, exponent2...],...]
     """
-    t = termMatrix
-    for i in range(1, len(t)):
+    t = [term[:] for term in termMatrix]
+    for i, term in enumerate(t, start=1):
         if i < len(t) - 1:
             for j in range(i+1, len(t)):
                 if t[i][1:] == t[j][1:]:
-                    t[i] = [t[i][0]+t[j][0]]+t[i][1:]
+                    t[i] = [t[i][0] + t[j][0]] + t[i][1:]
                     t[j][0] = 0
     # get rid of 0 terms
     t = [u for u in t if u[0] != 0]
     # get rid of extra variables
     if len(t[0]) > 0:
-        for i in range(len(t[0]) - 1, 0, -1):
+        for i in reversed(range(len(t[0]))):
             # in reverse so deletion doesn't affect index of subsequent variables
             extra = True
             if len(t) > 0:
