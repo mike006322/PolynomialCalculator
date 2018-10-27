4e1fbef4fe0e49d231cc454de9c2c645c0651103
Michael Angel <mike006322@gmail.com>
2018-10-26 17:47:39 -0700

    PEP8

--- multivariable/multivariable_polynomial_test.py
+++ multivariable/multivariable_polynomial_test.py
@@ -7,8 +7,8 @@ class TestParser(unittest.TestCase):
     def test__init__(self):
         self.assertEqual(Polynomial(0).termMatrix, [[' ']])
         t = [[' ', 'y', 'x'], [5.0, 1, 2], [24.0, 0, 0], [2.0, 0, 1], [1.0, 1, 0]]
-        T = Polynomial(t, char=2)
-        self.assertEqual(T.termMatrix, [[' ', 'y', 'x'], [1.0, 1, 2], [1.0, 1, 0]])
+        t = Polynomial(t, char=2)
+        self.assertEqual(t.termMatrix, [[' ', 'y', 'x'], [1.0, 1, 2], [1.0, 1, 0]])
 
     def test_clean(self):
         self.assertEqual(multivariable_polynomial.Polynomial.clean([[' ', 'x', 'y'], [3.0, 2, 0], [1.0, 1, 0], [5.0, 0, 0]]), [[' ', 'x'], [3.0, 2], [1.0, 1], [5.0, 0]])
@@ -49,59 +49,48 @@ class TestParser(unittest.TestCase):
         self.assertTrue(a * b == 0)
 
     def test_division(self):
-        # s = 'x^2y + xy^2 + y^2'
-        t = 'xy - 1'
-        e = 'y^2 - 1'
-        # S = Polynomial(s)
-        T = Polynomial(t)
-        E = Polynomial(e)
+        t = Polynomial('xy - 1')
+        s = Polynomial('y^2 - 1')
         a = Polynomial('x^2 - 1')
         b = Polynomial('x + 1')
         c = Polynomial('x - 1')
         self.assertTrue(a / b == c)
         self.assertRaises(ZeroDivisionError, lambda: a / 0)
         self.assertTrue(b / b == 1)
-        self.assertRaises(multivariable_polynomial.NonFactor, lambda: T / E)
+        self.assertRaises(multivariable_polynomial.NonFactor, lambda: t / s)
         a = Polynomial('2x^2 - 2', char=2)
         b = Polynomial('x + 1', char=2)
         self.assertTrue(a / b == 0)
 
 
     def test_LT(self):
-        t = 'xy - 1'
-        T = Polynomial(t)
-        s = 'x^2y + xy^2 + y^2'
-        S = Polynomial(s)
-        self.assertEqual(T.LT(), Polynomial('xy'))
-        self.assertEqual(S.LT(), Polynomial('x^2y'))
+        t = Polynomial('xy - 1')
+        s = Polynomial('x^2y + xy^2 + y^2')
+        self.assertEqual(t.LT(), Polynomial('xy'))
+        self.assertEqual(s.LT(), Polynomial('x^2y'))
 
     def test_divides(self):
-        t = 'xy - 1'
-        T = Polynomial(t)
-        s = 'x^2y + xy^2 + y^2'
-        S = Polynomial(s)
-        self.assertEqual(Polynomial.divides(T, S), True)
-        self.assertEqual(Polynomial.divides(S, T), False)
+        t = Polynomial('xy - 1')
+        s = Polynomial('x^2y + xy^2 + y^2')
+        self.assertEqual(Polynomial.divides(t, s), True)
+        self.assertEqual(Polynomial.divides(s, t), False)
 
     def test__iter__(self):
-        s = 'x^2y + xy^2 + y^2'
-        S = multivariable_polynomial.Polynomial(s)
+        S = Polynomial('x^2y + xy^2 + y^2')
         self.assertEqual([term for term in S], [Polynomial('x^2y'), Polynomial('xy^2'), Polynomial('y^2')])
 
     def test_mod_poly(self):
         t = [[' ', 'y', 'x'], [5.0, 1, 2], [24.0, 0, 0], [2.0, 0, 1], [1.0, 1, 0]]
-        T = Polynomial(t, char=2)
-        self.assertEqual(T.termMatrix, [[' ', 'y', 'x'], [1.0, 1, 2], [1.0, 1, 0]])
+        t = Polynomial(t, char=2)
+        self.assertEqual(t.termMatrix, [[' ', 'y', 'x'], [1.0, 1, 2], [1.0, 1, 0]])
 
     def test_combine_variables(self):
-        t = 'x'
-        T = multivariable_polynomial.Polynomial(t)
-        s = 'x^2y + xy^2 + y^2'
-        S = multivariable_polynomial.Polynomial(s)
+        t = Polynomial('x')
+        s = Polynomial('x^2y + xy^2 + y^2')
         res = (Polynomial([[' ', 'x', 'y'], [1.0, 2, 1], [1.0, 1, 2], [1.0, 0, 2]]), Polynomial([[' ', 'x', 'y'], [1.0, 1, 0]]))
-        self.assertEqual(Polynomial.combine_variables(S, T), res)
-        self.assertEqual(S.termMatrix, [[' ', 'x', 'y'], [1.0, 2, 1], [1.0, 1, 2], [1.0, 0, 2]])
-        self.assertEqual(T.termMatrix, [[' ', 'x'], [1.0, 1]])
+        self.assertEqual(Polynomial.combine_variables(s, t), res)
+        self.assertEqual(s.termMatrix, [[' ', 'x', 'y'], [1.0, 2, 1], [1.0, 1, 2], [1.0, 0, 2]])
+        self.assertEqual(t.termMatrix, [[' ', 'x'], [1.0, 1]])
 
 
 
