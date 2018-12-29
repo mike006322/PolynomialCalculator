64b329b2638d7189af4e4f48f54245cf175c42d9
Michael Angel <mike006322@gmail.com>
2018-12-28 20:14:20 -0800

    refactored tests

--- core/test_orderings.py
+++ core/test_orderings.py
@@ -6,15 +6,24 @@ class TestParser(unittest.TestCase):
 
     def test_order_lex(self):
         # -5x^3 + 7x^2z^2 + 4xy^2z + 4z^2
-        term_matrix = [[' ', 'x', 'y', 'z'], [-5, 3, 0, 0], [7, 2, 0, 2], [4, 1, 2, 1], [4, 0, 0, 2]]
-        self.assertEqual(grev_lex(term_matrix), [[' ', 'x', 'y', 'z'], [4, 1, 2, 1], [7, 2, 0, 2], [-5, 3, 0, 0], [4, 0, 0, 2]])
-        ordered_reverse_lex = [[' ', 'x', 'y', 'z'], [4, 0, 0, 2], [4, 1, 2, 1], [7, 2, 0, 2], [-5, 3, 0, 0]]
-        self.assertEqual(reverse_lex(term_matrix), ordered_reverse_lex)
-        ordered_lex = [[' ', 'x', 'y', 'z'], [-5, 3, 0, 0], [7, 2, 0, 2], [4, 1, 2, 1], [4, 0, 0, 2]]
-        self.assertEqual(order_lex(term_matrix), ordered_lex)
+        term_matrix1 = [[' ', 'x', 'y', 'z'], [-5, 3, 0, 0], [7, 2, 0, 2], [4, 1, 2, 1], [4, 0, 0, 2]]
+        res1 = [[' ', 'x', 'y', 'z'], [-5, 3, 0, 0], [7, 2, 0, 2], [4, 1, 2, 1], [4, 0, 0, 2]]
+        self.assertEqual(order_lex(term_matrix1), res1)
         term_matrix2 = [[' ', 'x', 'y'], [1.0, 2, 0], [1.0, 1, 0], [1.0, 0, 3], [1.0, 0, 1], [8.0, 0, 0]]
-        ordered_lex2 = [[' ', 'x', 'y'], [1.0, 2, 0], [1.0, 1, 0], [1.0, 0, 3], [1.0, 0, 1], [8.0, 0, 0]]
-        self.assertEqual(order_lex(term_matrix2), ordered_lex2)
+        res2 = [[' ', 'x', 'y'], [1.0, 2, 0], [1.0, 1, 0], [1.0, 0, 3], [1.0, 0, 1], [8.0, 0, 0]]
+        self.assertEqual(order_lex(term_matrix2), res2)
+
+    def test_reverse_lex(self):
+        # -5x^3 + 7x^2z^2 + 4xy^2z + 4z^2
+        term_matrix = [[' ', 'x', 'y', 'z'], [-5, 3, 0, 0], [7, 2, 0, 2], [4, 1, 2, 1], [4, 0, 0, 2]]
+        res = [[' ', 'x', 'y', 'z'], [4, 0, 0, 2], [4, 1, 2, 1], [7, 2, 0, 2], [-5, 3, 0, 0]]
+        self.assertEqual(reverse_lex(term_matrix), res)
+
+    def test_grev_lex(self):
+        # -5x^3 + 7x^2z^2 + 4xy^2z + 4z^2
+        term_matrix = [[' ', 'x', 'y', 'z'], [-5, 3, 0, 0], [7, 2, 0, 2], [4, 1, 2, 1], [4, 0, 0, 2]]
+        res = [[' ', 'x', 'y', 'z'], [4, 1, 2, 1], [7, 2, 0, 2], [-5, 3, 0, 0], [4, 0, 0, 2]]
+        self.assertEqual(grev_lex(term_matrix), res)
 
 
 if __name__ == '__main__':
