7b07a92751242cbe025fec26029ad110f5afd76c
Michael Angel <mike006322@gmail.com>
2019-04-13 14:06:57 -0700

    Added number classes

---
+++ core/numbers.py
@@ -0,0 +1,154 @@
+# This module defines number classes so that numbers can be manipulated and presented symbolically
+
+
+class Integer:
+    """
+    Integer class represents members of ring of integers, Z
+    division of integers that don't share common divisors yields a Rational number
+    """
+
+    def __init__(self, i):
+        self.value = int(i)
+
+    def __int__(self):
+        return self.value
+
+    def __eq__(self, other):
+        if type(other) == int:
+            if int(self) == other:
+                return True
+            else:
+                return False
+        else:
+            if self.value == other.value:
+                return True
+            else:
+                return False
+
+    def __ne__(self, other):
+        if self == other:
+            return False
+        else:
+            return True
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def __le__(self, other):
+        return self.value <= other.value
+
+    def __gt__(self, other):
+        return self.value > other.value
+
+    def __ge__(self, other):
+        return self.value >= other.value
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value)
+
+    def __add__(self, other):
+        return Integer(self.value + other.value)
+
+    def __sub__(self, other):
+        return Integer(self.value - other.value)
+
+    def __mul__(self, other):
+        return Integer(self.value * other.value)
+
+    def __pow__(self, power, modulo=None):
+        return Integer(self.value**(int(power)))
+
+    def __mod__(self, other):
+        return Integer(self.value % other.value)
+
+    def __floordiv__(self, other):
+        return Integer(self.value // other.value)
+
+    def __truediv__(self, other):
+        if self.value % other.value == 0:
+            return self//other
+        else:
+            return Rational(self, other)
+
+
+class Rational:
+    """
+    Rational class represents members of field of rational numbers, Q
+    """
+
+    def __init__(self, a, b=None):
+        if type(a) == str:
+            if '/' in a:
+                self.numerator = Integer(int(a[:a.find('/')]))
+                self.denominator = Integer(int(a[a.find('/') + 1:]))
+        else:
+            self.numerator = Integer(a)
+            if not b:
+                self.denominator = Integer(1)
+            else:
+                self.denominator = Integer(b)
+        Rational.normalize(self)
+
+    @staticmethod
+    def gcd(a, b):
+        if a >= b:
+            r = a % b
+            while r != 0:
+                a = b
+                b = r
+                r = a % b
+            return b
+        else:
+            return Rational.gcd(b, a)
+
+    @staticmethod
+    def normalize(q):
+        g = Rational.gcd(q.numerator, q.denominator)
+        q.numerator = q.numerator // g
+        q.denominator = q.denominator // g
+        if q.denominator < Integer(0):
+            q.denominator *= Integer(-1)
+            q.numerator *= Integer(-1)
+
+    def __int__(self):
+        return self.numerator // self.denominator
+
+    def __eq__(self, other):
+        if self.numerator == other.numerator and self.denominator == other.denominator:
+            return True
+
+    def __str__(self):
+        return str(self.numerator) + '/' + str(self.denominator)
+
+    def __repr__(self):
+        return str(self.numerator) + '/' + str(self.denominator)
+
+    def __mul__(self, other):
+        if type(other) == Rational:
+            return Rational(self.numerator*other.numerator, self.denominator*other.denominator)
+        else:
+            return self*Rational(other)
+
+    def __truediv__(self, other):
+        if type(other) == Rational:
+            return Rational(self.numerator*other.denominator, self.denominator*other.numerator)
+        else:
+            return self/Rational(other)
+
+    def __add__(self, other):
+        if type(other) == Rational:
+            return Rational(self.numerator*other.denominator + other.numerator*self.denominator, self.denominator*other.denominator)
+        else:
+            return self+Rational(other)
+
+    def __sub__(self, other):
+        if type(other) == Rational:
+            return Rational(self.numerator*other.denominator - other.numerator*self.denominator, self.denominator*other.denominator)
+        else:
+            return self-Rational(other)
+
+    def __pow__(self, power, modulo=None):
+        return Rational(self.numerator**power, self.denominator**power)
---
+++ core/test_numbers.py
@@ -0,0 +1,119 @@
+import unittest
+from core.numbers import *
+
+
+class TestInteger(unittest.TestCase):
+
+    def test__init__(self):
+        a = Integer(4)
+        self.assertEqual(a.value, 4)
+        with self.assertRaises(ValueError):
+            Integer('s')
+
+    def test__eq__(self):
+        self.assertEqual(Integer(2), Integer(2))
+        self.assertEqual(Integer(2), 2)
+        self.assertEqual(2, Integer(2))
+        self.assertTrue(Integer(3) != Integer(2))
+        self.assertFalse(Integer(2) != Integer(2))
+        self.assertFalse(Integer(3) == Integer(2))
+
+    def test_comparisons(self):
+        self.assertTrue(Integer(2) < Integer(3))
+        self.assertTrue(Integer(2) <= Integer(3))
+        self.assertTrue(Integer(3) > Integer(2))
+        self.assertTrue(Integer(3) >= Integer(2))
+
+    def test__int__(self):
+        self.assertEqual(int(Integer(2)), 2)
+
+    def test__str__(self):
+        a = Integer(4)
+        self.assertEqual(str(a), '4')
+
+    def test__add__(self):
+        a = Integer(2)
+        b = Integer(3)
+        self.assertEqual(a+b, Integer(5))
+
+    def test__sub__(self):
+        a = Integer(2)
+        b = Integer(3)
+        self.assertEqual(a-b, Integer(-1))
+
+    def test__mul__(self):
+        a = Integer(2)
+        b = Integer(3)
+        self.assertEqual(a*b, Integer(6))
+
+    def test__pow__(self):
+        a = Integer(2)
+        b = Integer(3)
+        self.assertEqual(a**b, Integer(8))
+
+    def test__mod__(self):
+        a = Integer(2)
+        b = Integer(3)
+        self.assertEqual(a % b, Integer(2))
+        self.assertEqual(b % a, Integer(1))
+
+    def test__floordiv__(self):
+        self.assertEqual(Integer(3) // Integer(2), 1)
+
+    def test__truediv__(self):
+        self.assertEqual(Integer(4)/Integer(2), Integer(2))
+        self.assertEqual(Integer(2)/Integer(4), Rational(1, 2))
+
+
+class TestRational(unittest.TestCase):
+
+    def test__init__(self):
+        q = Rational(1, 4)
+        self.assertEqual(q.numerator, 1)
+        self.assertEqual(q.denominator, 4)
+        self.assertEqual(Rational(2), Rational(2, 1))
+        self.assertEqual(Rational('1/4'), Rational(1, 4))
+
+    def test__str__(self):
+        q = Rational(1, 4)
+        self.assertEqual(str(q), '1/4')
+
+    def test_gcd_(self):
+        self.assertEqual(Rational.gcd(2, 4), 2)
+
+    def test_normalize(self):
+        q = Rational(1, 4)
+        p = Rational(2, 8)
+        Rational.normalize(p)
+        self.assertEqual(p, q)
+        q = Rational(-1, 4)
+        p = Rational(2, -8)
+        Rational.normalize(p)
+        self.assertEqual(p, q)
+        self.assertEqual(Rational(1, 4), Rational(2, 8))
+
+    def test__mul__(self):
+        q = Rational(1, 4)
+        p = Rational(3, 5)
+        self.assertEqual(p*q, Rational(3, 20))
+        self.assertEqual(p*2, Rational(6, 5))
+
+    def test__truediv__(self):
+        q = Rational(1, 4)
+        p = Rational(3, 5)
+        self.assertEqual(p/q, Rational(12, 5))
+
+    def test__add__(self):
+        self.assertEqual(Rational(1, 2) + Rational(3, 4), Rational(5, 4))
+        self.assertEqual(Rational(1, 2) + 1, Rational(3, 2))
+
+    def test__sub__(self):
+        self.assertEqual(Rational(1, 2) - Rational(3, 4), Rational(-1, 4))
+        self.assertEqual(Rational(1, 2) - 1, Rational(-1, 2))
+
+    def test__pow__(self):
+        self.assertEqual(Rational(3, 2)**2, Rational(9, 4))
+
+
+if __name__ == '__main__':
+    unittest.main()
