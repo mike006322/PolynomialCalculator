07cea23190a2b02e6de9fe6f3873848c3f2c10ad
Michael Angel <mike006322@gmail.com>
2019-05-08 10:21:59 -0700

    adds support to quadratic formula for less than 3 terms

--- core/formulas.py
+++ core/formulas.py
@@ -24,6 +24,7 @@ def solve(polynomial):
 
 
 def quadratic_formula(polynomial):
+    print(polynomial)
     """
     input is single-variable polynomial of degree 2
     returns zeros
@@ -34,7 +35,12 @@ def quadratic_formula(polynomial):
             return 0, float(-b/a)
         a, c = polynomial.term_matrix[1][0], polynomial.term_matrix[2][0]
         return float((-c/a)**.5), float(-(-c/a)**.5)
-    a, b, c = polynomial.term_matrix[1][0], polynomial.term_matrix[2][0], polynomial.term_matrix[3][0]
+    if len(polynomial.term_matrix) == 2:
+        a, b, c, = polynomial.term_matrix[1][0], 0, 0
+    elif len(polynomial.term_matrix) == 3:
+        a, b, c = polynomial.term_matrix[1][0], polynomial.term_matrix[2][0], 0
+    else:
+        a, b, c = polynomial.term_matrix[1][0], polynomial.term_matrix[2][0], polynomial.term_matrix[3][0]
     ans1 = (-b + (b**2 - 4*a*c)**.5)/2*a
     ans2 = (-b - (b**2 - 4*a*c)**.5)/2*a
     if ans1 == ans2:
