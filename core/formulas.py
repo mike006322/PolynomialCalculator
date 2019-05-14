e0d7f5adc795e3a06085eeae49f077ab397d0d89
Michael Angel <mike006322@gmail.com>
2019-05-14 08:34:45 -0700

    removes "float()" from answer return to allow for complex

--- core/formulas.py
+++ core/formulas.py
@@ -24,6 +24,7 @@ def solve(polynomial):
 
 
 def quadratic_formula(polynomial):
+    print(polynomial)
     """
     input is single-variable polynomial of degree 2
     returns zeros
@@ -43,8 +44,8 @@ def quadratic_formula(polynomial):
     ans1 = (-b + (b**2 - 4*a*c)**.5)/2*a
     ans2 = (-b - (b**2 - 4*a*c)**.5)/2*a
     if ans1 == ans2:
-        return float(ans1)
-    return float(ans1), float(ans2)
+        return ans1
+    return ans1, ans2
 
 
 def isclose(a, b, rel_tol=1e-09, abs_tol=0.0001):
