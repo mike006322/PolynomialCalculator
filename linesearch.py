53fb9a5ee5a98ef3930aca37ddc3b286328bf642
Michael Angel <mike006322@gmail.com>
2019-10-03 13:08:53 -0700

    fixes bug in parser

--- linesearch.py
+++ linesearch.py
@@ -6,11 +6,32 @@ def euclidean_norm(vector):
     return (sum([component**2 for component in vector]))**.5
 
 
+def line_search(f, initial_point, descent_direction_function):
+    """
+    Input polynomial f, initial point, descent direction p, and descent direction function.
+    Minimizes the polynomial.
+    Returns iterations that stop at the local minimum.
+    x_(k+1) = x_k + alpha_k*p_k
+    """
+    iterations = []
+    x = initial_point
+    norm = euclidean_norm
+    while abs(f(*x)) > 10**(-8) and norm(f.grad(*x)) > 10**(-8):
+        p = descent_direction_function(f, x)  # Newton's or Steepest Descent
+        alpha = backtracking_algorithm(f, x, p)
+        iterations.append('x_k: ' + str(x) + ', f(x_k): ' + str(f(*x)) + ', p: ' +
+                          str(p) + ', alpha_k: ' + str(alpha))
+        # x = x + alpha*p
+        x = vector_plus_vector(x, constant_times_vector(alpha, p))
+    return iterations
+
+
 def steepest_descent(f, x):
     """
     input polynomial f, tuple x representing point
     returns the direction vector, p, of steepest descent
     p = -f.grad(x)/euclidean_norm(f.grad(x))
+    Using steepest descent the linesearch will converge to local minimum but at linear speed (slow)
     """
     p = f.grad(*x)
     norm = euclidean_norm
@@ -22,54 +43,15 @@ def steepest_descent(f, x):
 
 def newtons_algorithm(f, x):
     """
-    input polynomial f, tuple x representing point
-    returns the direction vector, p, of steepest descent
+    Input polynomial f and tuple x representing point.
+    Returns the direction vector, p, of steepest descent
     p = -f.hessian(x)^(-1) * f.grad(x)
+    Using Newton's algorithm the linesearch might not converge but it runs at quadratic speed (fast)
     """
     inverse_hessian = get_matrix_inverse(f.hessian(*x))
     p = matrix_times_vector(inverse_hessian, f.grad(*x))
     p = constant_times_vector(-1, p)
     return p
-
-
-def line_search_steepest_descent(f, initial_point):
-    """
-    input f: polynomial, p: descent direction, initial point
-    minimizes the polynomial
-    return a local minimum
-    x_(k+1) = x_k + alpha_k*p_k
-    """
-    x = initial_point
-    norm = euclidean_norm
-    while abs(f(*x)) > 10**(-8) and norm(f.grad(*x)) > 10**(-8):
-        p = steepest_descent(f, x)
-        alpha = backtracking_algorithm(f, x, p)
-        # x = x + alpha*p
-        x = vector_plus_vector(x, constant_times_vector(alpha, p))
-        print('x_k = ', x)
-        print('f(*x) = ', f(*x))
-        print('norm(f.grad(*x)) = ', norm(f.grad(*x)))
-    return x
-
-
-def line_search_newtons_algorithm(f, initial_point):
-    """
-    input f: polynomial, p: descent direction, initial point
-    minimizes the polynomial
-    return a local minimum
-    x_(k+1) = x_k + alpha_k*p_k
-    """
-    x = initial_point
-    norm = euclidean_norm
-    while abs(f(*x)) > 10**(-8) and norm(f.grad(*x)) > 10**(-8):
-        p = newtons_algorithm(f, x)
-        alpha = backtracking_algorithm(f, x, p)
-        # x = x + alpha*p
-        x = vector_plus_vector(x, constant_times_vector(alpha, p))
-        print('x_k = ', x)
-        print('f(*x) = ', f(*x))
-        print('norm(f.grad(*x)) = ', norm(f.grad(*x)))
-    return x
 
 
 def backtracking_algorithm(f, x_k, p):
@@ -81,32 +63,77 @@ def backtracking_algorithm(f, x_k, p):
     c = 10**(-4)
     # while f(x_k + alpha*p) > f(x_k) + c*alpha*p_transpose*f.grad(x_k):
     while f(*vector_plus_vector(x_k, constant_times_vector(alpha, p))) > f(*x_k) + c*alpha*vector_times_vector(p, f.grad(*x_k)):
-        # print('alpha = ', alpha)
         alpha = rho*alpha
     return alpha
-    
 
-# TODO: Modified Newton conditions
-# Algorithm to check if matrix is positive definite
-# Matrix class - maybe with cython optimization
-# Lengraneg Interpolation
 
-# Wolfe conditions is inherent in backtracking line search
+def find_step_length(phi):
+    """
+    iterates to find an appropriate step length
+    returns alpha_star
+    """
+# Set alpha_0 = 0, choose alpha_1 > 0, alpha_max, c_1, and c_2, i = 1
+# while True:
+#   Compute phi(alpha_i)
+#   if ( phi(alpha_i) > phi(0) + c_1*alpha_i*phi'(0) ) or (phi(alpha_i) >= phi(alpha_(i-1)) and i > 1):
+#       alpha_star = zoom(alpha_(i-1), alpha_i)
+#       break # return alpha_star?
+#   Compute phi'(alpha_i)
+#   if |phi'(alpha_i)| <= -c_2phi'(0):
+#       alpha_star = alpha_i
+#       break
+#   if phi'(alpha_i) >= 0:
+#       alpha_star = zoom(alpha_i, alpha_(i-1))
+#       break
+#   Choose αi+1 ∈ [alpha_i, alpha_max]
+#   i += 1
 
-# we know the following for nice enough f: 
-# if we ensure p_k is not perpendicular to grad(x_k), i.e. cos(theta_k) >= delta > 0
-# compute cos(theta_k) in each iteration, and turn p_k in the steepest descent direction if
-# needed (cos(theta_k) < delta)
-# and alpha_k statisfies the wolve conditions
-# then we have a globally convergent algorithm
 
-# we can perform angle tests (|cos(theta_k)| > delta) and subsequently "turn" p_k to ensure global convergence
-# however, this will slow down convergence rate
-# they brak quasi newton methors which are important for large problems
+def zoom(phi, alpha_low, alpha_high):
+    """
+    :param phi:
+    :return:
+    """
+    pass
+# while True:
+#   Interpolate to find alpha_j between alpha_low and alpha_high
+#   Compute phi(alpha_j)
+#   if ( phi(alpha_j) > phi(0) + c_1*alpha_j*phi′(0) ) or (phi(alpha_j) >= phi(alpha_low) ):
+#       alpha_high = alpha_j
+#   else:
+#       Compute phi'(alpha_j)
+#       if |phi'(alpha_j)| <= -c_2*phi'(0):
+#           alpha_star = alpha_j
+#           return alpha_star
+#       if phi'(alpha_j)*(alpha_high - alpha_low) >= 0:
+#           alpha_high = alpha_low
+#       alpha_low = alpha_j
+
+
+def print_results(iterations):
+    print('Number of iterations: ', len(iterations))
+    if len(iterations) > 20:
+        for line in iterations[0:11]:
+            print(line)
+        print('  .')
+        print('  .')
+        print('  .')
+        for line in iterations[-10:]:
+            print(line)
+    else:
+        for line in iterations:
+            print(line)
+
+
+def main():
+    f = Polynomial('(x1+x2^2)^2')
+    iterations = line_search(f, (1, 1), steepest_descent)
+
+    # rosenbrock_function = Polynomial('100(x2-x1^2)^2 + (1-x1)^2')
+    # iterations = line_search(rosenbrock_function, (1.2, 1.2), newtons_algorithm)
+    # iterations = line_search(rosenbrock_function, (1.2, 1.2), steepest_descent)
+    print_results(iterations)
 
 
 if __name__ == '__main__':
-    # rosenbrock_function = Polynomial('100(x2 − x1^2)^2 + (1-x_1)^2')
-    rosenbrock_function = Polynomial('100x2^2 - 200x1^2x2 + 100x1^4 + 1-2x1 + x1^2')
-    print(line_search_newtons_algorithm(rosenbrock_function, (-1.2, 1)))
-    # print(line_search_steepest_descent(rosenbrock_function, (-1.2, 1)))
+    main()
