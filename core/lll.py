efc5a7283a73559b50eeee82ff328e2f71138a55
Michael Angel <mike00632@gmail.com>
2019-11-18 16:54:29 -0800

    adds LLL algorithm

---
+++ core/lll.py
@@ -0,0 +1,33 @@
+from core.vector import Vector
+from core.polycalc_numbers import Rational
+from typing import List, Sequence
+from core.matrix import *
+
+
+def reduction(basis, delta):
+    n = len(basis)
+    basis = list(map(Vector, basis))
+    orthogonal = gram_schmidt(basis)
+
+    def mu(i: int, j: int) -> Rational:
+        return orthogonal[j].proj_coeff(basis[i])
+
+    k = 1
+    while k < n:
+        for j in range(k - 1, -1, -1):
+            mu_kj = mu(k, j)
+            if abs(mu_kj) > 0.5:
+                basis[k] = basis[k] - basis[j] * round(mu_kj)
+                orthogonal = gram_schmidt(basis)
+        if orthogonal[k].sdot() >= (delta - mu(k, k - 1) ** 2) * orthogonal[k - 1].sdot():
+            k += 1
+        else:
+            basis[k], basis[k - 1] = basis[k - 1], basis[k]
+            orthogonal = gram_schmidt(basis)
+            k = max(k - 1, 1)
+
+    return [list(map(Rational, b)) for b in basis]
+
+
+if __name__ == '__main__':
+    pass
---
+++ core/test_lll.py
@@ -0,0 +1,19 @@
+import unittest
+from core.lll import *
+
+
+class TestLLL(unittest.TestCase):
+
+    def test_lll(self):
+        m = reduction([[1, 1, 1], [-1, 0, 2], [3, 5, 6]], 0.75)
+        res = [[0, 1, 0], [1, 0, 1], [-1, 0, 2]]
+        self.assertEqual(m, res)
+        m = [[105, 821, 404, 328], [881, 667, 644, 927], [181, 483, 87, 500], [893, 834, 732, 441]]
+        m = [list(map(Rational, x)) for x in m]
+        m = reduction(m, 0.75)
+        res = [[76, -338, -317, 172], [88, -171, -229, -314], [269, 312, -142, 186], [519, -299, 470, -73]]
+        self.assertEqual(m, res)
+
+
+if __name__ == '__main__':
+    unittest.main()
