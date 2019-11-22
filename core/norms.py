4d888e0b4612b45aec82310b124647c5e15bba22
Michael Angel <mike00632@gmail.com>
2019-11-22 15:05:54 -0800

    adds weighted frobenius norm

--- core/norms.py
+++ core/norms.py
@@ -2,5 +2,9 @@ def euclidean_norm(vector):
     return (sum([component ** 2 for component in vector])) ** .5
 
 
+def weighted_frobenius_norm(matrix):
+    return (sum([sum([abs(a) ** 2 for a in row]) for row in matrix])) ** .5
+
+
 if __name__ == '__main__':
     pass
--- core/test_norms.py
+++ core/test_norms.py
@@ -1,11 +1,18 @@
 import unittest
 from core.norms import *
+from core.matrix import Matrix
 
 
 class TestLattice(unittest.TestCase):
 
     def test_euclidean_norm(self):
         self.assertEqual(euclidean_norm([3, 4]), 5)
+
+    def test_weighted_frobenius_norm(self):
+        m = Matrix.identity(2)
+        self.assertEqual(weighted_frobenius_norm(m), 2 ** .5)
+        m = Matrix([[-1, 1], [0, 2]])
+        self.assertEqual(weighted_frobenius_norm(m), 6 ** .5)
 
 
 if __name__ == '__main__':
